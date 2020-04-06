de4a74bc88b4d9183a57d5e43f68d747782cbdd1
Alex L <macbookpro@macbooks-mbp.home>
2020-04-06 15:25:25 +0100

    test - page shows bookmarks - fail

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -4,3 +4,10 @@ feature 'From index I can view bookmarks' do
     expect(page).to have_content "Bookmarks"
   end
 end
+
+feature 'From index I can view bookmarks' do
+  scenario 'bookmarks are displayed' do
+    visit ('/bookmarks')
+    expect(page).to have_content "'www.google.co.uk'"
+  end
+end
